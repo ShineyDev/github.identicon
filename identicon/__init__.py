d2a83dd599e0245c25050bb0f27eff0c130f64c4
Riley Shaw <contact@shiney.dev>
2020-12-10 16:39:52 +0000

    add version information

--- identicon/__init__.py
+++ identicon/__init__.py
@@ -16,4 +16,11 @@
     limitations under the License.
 """
 
+import collections
+
 from identicon.identicon import Identicon
+
+
+_VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro releaselevel serial")
+version_info = _VersionInfo(1, 0, 0, "final", 0)
+version = "1.0.0"
