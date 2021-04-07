5b1471bac5051fe0ecc3a3263a04688ed7e7fc96
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-04-08 00:32:41 +0100

    patch stubs

--- identicon/__init__.pyi
+++ identicon/__init__.pyi
@@ -1,1 +1,14 @@
+from typing import NamedTuple
+
 from identicon.identicon import Identicon as Identicon
+
+
+class _VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release: str
+    serial: int
+
+version: str = ...
+version_info: _VersionInfo = ...
